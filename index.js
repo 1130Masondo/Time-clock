aef5d554d0c155276986bf669b99ae3b861fc5f3
1130Masondo <133394928+1130Masondo@users.noreply.github.com>
2024-10-13 12:02:16 +0200

    Add files via upload
    add javascript

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-setInterval(function() {
+function updateTime() {
+
     let parisElement = documentquerySelector("#paris");
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".Time");
-    let parisTime = moment().tz("American/paris");
+    if (parisElement) {
+        let parisDateElement = parisElement.querySelector(".date");
+        let parisTimeElement = parisElement.querySelector(".Time");
+        let parisTime = moment().tz("American/paris");
 
-    parisDateElement.innerHTML = parisTime.formant("MMM DO YYYY");
-    parisTimeElement.innerHTML = parisTime.formant("h:mm:ss [<small>]A[<small>]"
+        parisDateElement.innerHTML = parisTime.formant("MMM DO YYYY");
+        parisTimeElement.innerHTML = parisTime.formant("h:mm:ss [<small>]A[<small>]"
 
-    );
+        );
 
-}, 1000);
+    }
+
+}
+
 
 
 let pretoriaElement = documentquerySelector("#pretoria");
@@ -19,3 +24,27 @@ let pretoriaTime = moment().tz("africa/johennerburg");
 
 pretoriaDateElement.innerHTML = pretoriaTime.formant("MMM DO YYYY");
 pretoriaTimeElement.innerHTML = pretoriaTime.formant("h:mm:ss [<small>]A[<small>]");
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+     <div>
+      <h2>${cityTimeZone}</h2>
+      <div class="date">${cityTime.formant("MMM DO YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.formant("h:mm:ss")} <smal>${cityTime.formant("A")}</small> </div>
+    </div>
+    
+    `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
